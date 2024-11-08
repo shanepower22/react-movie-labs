1f58f7ce60ef57dac2b84c65e793de6960a092eb
Shane Power <20102592@mail.wit.ie>
2024-11-08 12:52:19 +0000

    Show movie reviews excerpts; Refactor Details page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-
+import { getMovies } from "../api/tmdb-api";
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
   const favorites = movies.filter(m => m.favorite)
@@ -14,16 +14,9 @@ const HomePage = (props) => {
   };
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        return json.results;
-      })
-      .then((movies) => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
 
   return (
--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,84 +1,32 @@
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
-import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
-import Grid from "@mui/material/Grid2";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        // console.log(movie)
-        setMovie(movie);
-      });
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
-
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        // console.log(images)
-        setImages(images);
-      });
-      // eslint-disable-next-line
-  }, []);
 
   return (
     <>
       {movie ? (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid size={{xs: 3}}>
-              <div sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-              }}>
-                <ImageList
-    sx={{
-      height: "100vh",
-    }}
-    cols={1}
-  >
-    {images.map((image) => (
-      <ImageListItem
-      key={image.file_path}
-      cols={1}
-      >
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-          alt={image.file_path}
-        />
-      </ImageListItem>
-    ))}
-  </ImageList>
-              </div>
-            </Grid>
-            <Grid size={{xs: 9}}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
 };
 
-export default MoviePage;   
+export default MoviePage;

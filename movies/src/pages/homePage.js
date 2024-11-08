63e0afa5cfee2516d3dd33f4258062bded71774c
Shane Power <20102592@mail.wit.ie>
2024-11-08 12:37:55 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -24,6 +24,13 @@ const HomePage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -51,8 +58,8 @@ const HomePage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
-      </Grid>
+    <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+          </Grid>
     </Grid>
   );
 };
